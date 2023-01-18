64f6d221605fc95f44376e0093c9a1e4ac397c42
Aya Shahin <66850776+AyaMShahin@users.noreply.github.com>
2023-01-18 20:46:08 +0200

    commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// const imgDiv = document.querySelector(".imgs");
+// const imgs = document.querySelectorAll(".hero");
+// const image = this;
+// imgDiv.addEventListener("mousemove", moving);
+
+// function moving(e) {
+//   imgs.forEach(function (move) {
+//     let movingValue = move.getAttribute("data-value");
+//     let moveX = e.clientX - movingValue;
+//     let moveY = e.clientY - movingValue;
+//     move.style.transform =
+//       "translateX(" + moveX + "px) translateY(" + moveY + "px)";
+//   });
+// }
